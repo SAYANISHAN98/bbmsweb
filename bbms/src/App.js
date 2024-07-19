6eb4e813e9a87fff73fd0394e03f3fb7e4251066
sivavithu <sivavithu15@live.com>
2024-07-19 14:07:12 +0530

    login
    login

--- bbms/src/App.js
+++ bbms/src/App.js
@@ -1,4 +1,8 @@
-import { BrowserRouter,Route, Router,Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthProvider from "./providers/AuthProvider";
+import PrivateRoute from "./routes/privateRoute";
+import PublicRoute from "./routes/publicRoute";
+
 import Login from "./project/login";
 import Layout from "./project/layout";
 import User from "./project/user";
@@ -9,36 +13,32 @@ import Update from "./project/Form/update/Update";
 import ViewDetail from "./project/ViewDetail";
 import Notfound from "./Notfound";
 
-
 function App() {
   return (
-  
-  
-   
-     <BrowserRouter>
-    
-      <Routes>
-      <Route path="/" element={<Layout />}>
-      <Route path="/ViewDetail" element={<ViewDetail />}/>
-      <Route path="/Add" element={<Add />}/>
-      <Route path="/Update" element={<Update />}/>
-      <Route index element={<Home />}/>
-          <Route path="/Login" element={<Login />}/>
-    
-            <Route path="/user" element={<User />}/>
-            <Route path="Stock" element={<Stock />}/>
-            <Route path='*' element={<Notfound/>}/>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+       
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/" element={<Layout />}>
+              <Route path="/ViewDetail" element={<ViewDetail />} />
+              <Route path="/Add" element={<Add />} />
+              <Route path="/Update" element={<Update />} />
+              <Route index element={<Home />} />
+              <Route path="/user" element={<User />} />
+              <Route path="/Stock" element={<Stock />} />
+              <Route path="*" element={<Notfound />} />
             </Route>
-
-          
-          
+          </Route>
+          <Route path="/login" element={<PublicRoute />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
         
-      </Routes>
+        </Routes>
+       
       </BrowserRouter>
-     
+      </AuthProvider>
    
-
-
   );
 }
 
