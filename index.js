8e94b2985c4a97957b297dd864b5a1ea4f461c64
SAYANISHAN98 <87234358+SAYANISHAN98@users.noreply.github.com>
2024-07-22 20:31:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-// Import dependencies
+// index.js (located in project-root)
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'bbms/src/App.js';  // Adjust the path if necessary
+import App from './bbms/src/App';  // Adjust this path if necessary
 
-// Create a root element
 const rootElement = document.getElementById('root');
 
-// Render the App component
 ReactDOM.createRoot(rootElement).render(
-  
+  <React.StrictMode>
     <App />
-  
+  </React.StrictMode>
 );
+
