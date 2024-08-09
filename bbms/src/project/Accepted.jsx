4c3a9feadbc19cac2d8a5a4efac6beb4c22a66fc
SAYANISHAN98 <87234358+SAYANISHAN98@users.noreply.github.com>
2024-08-09 10:43:04 +0530

    Deleted one file

--- bbms/src/project/Accepted.jsx
+++ bbms/src/project/Accepted.jsx
@@ -17,14 +17,7 @@ export default function Accepted() {
     <div className='flex flex-col items-center justify-center w-full mx-4 space-y-2 lg:w-full'>
      <div className='w-4/5'>
      <div className='flex justify-between mt-8'>
-          {/* <div className='flex justify-start'>
-            <button
-              onClick={() => navigate('/Finddonor')}
-              className='font-bold text-white bg-red-500 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-1 px-8 rounded-xl text-lg'
-            >
-              Finddonor
-            </button>
-          </div> */}
+          
           <div className='flex justify-center'>
             <button
               onClick={() => navigate('/Finddonor')}
@@ -58,7 +51,7 @@ export default function Accepted() {
                   <td className='px-2 py-2'>{userData.lastdonationdate}</td>
                   <td className='px-2 py-2 space-x-2'>
                   <button
-                      onClick={() => navigate('/ViewDonor')}
+                      onClick={() => navigate('/ViewDetail')}
                       className='font-bold text-white bg-red-500 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-1 px-4 rounded-xl text-lg'
                     >
                       View
