7e3f7c6780fdcb59ab143503ab455d58b95c8868
SAYANISHAN98 <87234358+SAYANISHAN98@users.noreply.github.com>
2024-12-17 22:29:42 +0530

    Update donordonations.jsx

--- bbms/src/project/donordonations.jsx
+++ bbms/src/project/donordonations.jsx
@@ -141,18 +141,14 @@ export default function Donordonations() {
           setError('You can only donate after 120 days from your last donation.');
           return;
         } else {
-// <<<<<<< test
-          // If donation is allowed, navigate to the donation page
+
           navigate(`/Donordonations/Donate/${profileData.id}`);
-// =======
-//           // If donation is allowed (more than 120 days), navigate to the donation page
-//           navigate(`/Donate/${profileData.id}`);
-// >>>>>>> main
+
           setModalOpen(false); // Close the modal after navigation
         }
       } else {
         // No donation record found, donor can donate
-        navigate(`/Donate/${profileData.id}`);
+        navigate(`/Donordonations/Donate/${profileData.id}`);
         setModalOpen(false); // Close the modal after navigation
       }
     }
