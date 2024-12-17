e13b1d8735c67ce354b30a84c5c098bfb6105999
SAYANISHAN98 <87234358+SAYANISHAN98@users.noreply.github.com>
2024-12-17 11:44:29 +0530

    Update donordonations.jsx

--- bbms/src/project/donordonations.jsx
+++ bbms/src/project/donordonations.jsx
@@ -238,7 +238,7 @@ export default function Donordonations() {
                     <td className="px-6 py-4 whitespace-nowrap">{donation.location}</td>
                     <td className="p-2 space-x-2">
                       <button
-                        onClick={() => navigate(`/viewdonations/${donation.id}`)}
+                        onClick={() => navigate(`/Donordonations/viewdonations/${donation.id}`)}
                         className="font-bold text-white bg-red-500 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-1 px-4 rounded-md ">
                         View
                       </button>
